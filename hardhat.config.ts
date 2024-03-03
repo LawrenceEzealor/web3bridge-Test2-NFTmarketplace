29451146cd0a2ed808f90102a240ca36dacda60d
Lawrence Ezealor <ezealor20@gmail.com>
2024-03-03 22:30:18 +0100

    updated deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,39 @@
-import { HardhatUserConfig } from "hardhat/config";
+// import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: ".env" });
 
-const config: HardhatUserConfig = {
+const ALCHEMY_MAINNET_API_KEY_URL = process.env.ALCHEMY_MAINNET_API_KEY_URL;
+
+const ALCHEMY_SEPOLIA_API_KEY_URL = process.env.ALCHEMY_SEPOLIA_API_KEY_URL;
+
+const ALCHEMY_MUMBAI_API_KEY_URL = process.env.ALCHEMY_MUMBAI_API_KEY_URL;
+
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY;
+
+module.exports = {
   solidity: "0.8.24",
-};
+  networks: {
+    hardhat: {
+      // forking: {
+      //   url: ALCHEMY_MAINNET_API_KEY_URL,
+      // }
+    },
+    sepolia: {
+      url: ALCHEMY_SEPOLIA_API_KEY_URL,
+      accounts: [ACCOUNT_PRIVATE_KEY],
+    },
+    // mumbai: {
+    //   url: ALCHEMY_MUMBAI_API_KEY_URL,
+    //   accounts: [ACCOUNT_PRIVATE_KEY],
+    // }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+    //   apiKey:{
+    //   polygonMumbai: POLYGONSCAN_API_KEY,
+    // }
+  },
 
-export default config;
+  lockGasLimit: 200000000000,
+  gasPrice: 10000000000,
+};
